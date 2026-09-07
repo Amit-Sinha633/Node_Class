12b00982ef8dbca8bfece6bc2eaef9a867b75dbc
Amit-Sinha633 <tataisinha2006@gmail.com>
2026-09-08 01:04:02 +0530

    advance login using accessToken and refreshToken

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import express from "express"
 import connectionDB from "./src/db/user.db.js"
 import router from "./src/routes/user.route.js"
+import cookieParser from "cookie-parser"
+
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
 connectionDB()
 
 app.use("/app/v1/users",router)
-const Port = 5000
+const Port = process.env.PORT
+
 app.listen(Port,()=>{
     console.log(`this is my localhost ${Port}`)
 })
--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,8 @@
 import User from "../model/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
 
 const registerUser = async (req, res) => {
   // 200 => successfull , 201 => created ,400 => Bad request, 403=> forbidden , 404 => not found , 500 => server error
@@ -47,7 +50,7 @@ const logInUser = async (req, res) => {
       });
     }
     // email ta check korbe user already register ache kine
-    const existingUser = await User.findOne({ email })
+    const existingUser = await User.findOne({ email });
     // jodi na thake thole amra error show korbo
     if (!existingUser) {
       return res.status(400).json({
@@ -65,8 +68,33 @@ const logInUser = async (req, res) => {
         msg: "Email or password is not correct",
       });
     }
+    // accessToken ar refreshToken generate korabo 
+      const accessToken = await jwt.sign(
+        {id: existingUser._id},
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: process.env.ACCESS_TOKEN_EXPIARY}
+      )
+
+      const refreshToken = await jwt.sign(
+        {id: existingUser._id},
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: process.env.REFRESH_TOKEN_EXPIARY}
+      )
+
+    // tarpor refreshToken ta ke database save rakhbo 
+    existingUser.refreshToken = refreshToken
+    await existingUser.save()
+    //  ar user k accessToken ar refreshToken cookies ar modha pathye dabo
+    const options = {
+      httpOnly: true,
+      secure: true
+    }
+
     // login korte dabo ar user info ta pathiye dabo response a
-    return res.status(200).json({
+    return res.status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json({
       msg: "User logIn successfully",
       data: existingUser,
     });
